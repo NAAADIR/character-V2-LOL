1e4cb6b52209e9bb415dd5eab4c825fd099bef32
NAAADIR <115558595+NAAADIR@users.noreply.github.com>
2023-08-04 10:32:47 +0200

    feat(navebar): fix wording + add icone github

--- src/components/navbar/ScrollTop.tsx
+++ src/components/navbar/ScrollTop.tsx
@@ -1,29 +1,17 @@
 import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import CssBaseline from '@mui/material/CssBaseline';
 import useScrollTrigger from '@mui/material/useScrollTrigger';
 import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
 import Fab from '@mui/material/Fab';
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 import Fade from '@mui/material/Fade';
 
 interface Props {
-  /**
-   * Injected by the documentation to work in an iframe.
-   * You won't need it on your project.
-   */
   window?: () => Window;
   children: React.ReactElement;
 }
 
 export function ScrollTop(props: Props) {
   const { window } = props;
-  // Note that you normally won't need to set the window ref as useScrollTrigger
-  // will default to window.
-  // This is only being set here because the demo is in an iframe.
   const trigger = useScrollTrigger({
     target: window ? window() : undefined,
     disableHysteresis: true,
@@ -50,7 +38,7 @@ export function ScrollTop(props: Props) {
         role="presentation"
         sx={{ position: 'fixed', bottom: 16, right: 16 }}
       >
-       <Fab size="small" aria-label="scroll back to top">
+      <Fab size="small" aria-label="scroll back to top">
           <KeyboardArrowUpIcon className='up-arrow-icon'/>
         </Fab>
       </Box>
